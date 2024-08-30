4247947847835246bceb4a259860e1db8ac769fd
samriddhik562 <samriddhik562@gmail.com>
2024-08-30 10:26:39 -0400

    created homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Home</h1>
+          <p>Welcome to <strong>The SRV Tech</strong> - the hiring solution to your job hunt!</p>
+        </div>
+    );
+  };
+  
+  export default Home;
