3052ed9512978cc395158b01f4b82975f848714c
Reggie Calhoun <drauge2208@gmail.com>
2024-09-03 18:06:00 +0000

    Added login features to pages

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,28 @@
 const LoginPage = () =>{
+
+    const handleInputChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setUser({ ...user, [name]: value });
+    };
+    
     return(
     <div className='container'>
     <h1>Sign In</h1>
     <form>
         <label htmlFor="usernameInput">Username:</label>
-        <input type="text" />
+        <input 
+            type="text"
+            onChange={handleInputChange}
+            value={user.username || ''}
+        />
     <br/>
         <label htmlFor="password">Password:</label>
-        <input type="password" />
+        <input 
+            type="password" 
+            onChange={handleInputChange}
+            value={user.password || ''}
+        />
     <br />
         <button type="submit">Login</button>
     </form>
