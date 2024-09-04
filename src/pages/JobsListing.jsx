ce5a3054c477409597f63a06e9839f48a0a9ebaa
Reggie Calhoun <drauge2208@gmail.com>
2024-09-04 14:32:23 +0000

    Changed Jobs Listing header

--- src/pages/JobsListing.jsx
+++ src/pages/JobsListing.jsx
@@ -1,7 +1,7 @@
 export default function JobsListing() {
     return (
       <div>
-        <h1> Jobs Available </h1>
+        <h1> Jobs </h1>
           <p>All Jobs with their details and openings are below</p>
       </div>
     )
