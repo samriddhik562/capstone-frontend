32f95a18dd18498606bab42fd8a740934fbd99df
samriddhik562 <samriddhik562@gmail.com>
2024-09-04 15:10:40 +0000

    copied files

--- src/pages/JobsListing.jsx
+++ src/pages/JobsListing.jsx
@@ -1,7 +1,8 @@
 export default function JobsListing() {
-    return (
-      <div>
-        <h1> Jobs </h1>
-      </div>
-    )
+  return (
+    <div>
+      <h1> Jobs </h1>
+        <p>All Jobs with their details and openings are below</p>
+    </div>
+  )
 }
