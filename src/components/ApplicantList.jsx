5fd8d6a154bc50122eaeadf4062ac7bb1f7e5fbd
samriddhik562 <samriddhik562@gmail.com>
2024-09-06 13:13:59 +0000

    path change

--- src/components/ApplicantList.jsx
+++ src/components/ApplicantList.jsx
@@ -3,7 +3,8 @@ import * as API from '../Server.js'
 
 export default function ApplicantList(){
 const [applications, setApplications] = useState([]);
-
+const [status1, setStatus1] = useState("Pending")
+const [status2, setStatus2] = useState("Pending")
   /*useEffect(() => {
     const pullApplications = () => {
         API.getApplications(2, (err, data) => {
@@ -17,6 +18,8 @@ const [applications, setApplications] = useState([]);
       };
     pullJobs();
     }, []);*/
+
+
     return(
         <>
         <h1>Applicants</h1>
@@ -31,6 +34,8 @@ const [applications, setApplications] = useState([]);
                         <th>Date Applied</th>
                         <th>&nbsp;</th>
                         <th>Status</th>
+                        <th>&nbsp;</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -38,13 +43,21 @@ const [applications, setApplications] = useState([]);
                         <td>John Doe</td><td>&nbsp;</td>
                         <td>Very Experienced</td><td>&nbsp;</td>
                         <td>2024-09-06</td><td>&nbsp;</td>
-                        <td>Pending</td>
+                        <td>{status1}</td>
+                        <td>
+                            <button onClick={()=>setStatus1("Accept")}>Accept</button>
+                            <button onClick={()=>setStatus1("Reject")}>Reject</button>
+                        </td>
                     </tr>
                     <tr>
                         <td>Cindy Loo</td><td>&nbsp;</td>
                         <td>Professionally Experienced</td><td>&nbsp;</td>
                         <td>2024-08-06</td><td>&nbsp;</td>
-                        <td>Pending</td>
+                        <td>{status2}</td>
+                        <td>
+                        <button onClick={()=>setStatus2("Accept")}>Accept</button>
+                            <button onClick={()=>setStatus2("Reject")}>Reject</button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
